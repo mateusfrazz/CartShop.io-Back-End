db73a685dd9c6da9b978f49c1c4b23c51652ee0f
mateus <mateusfrazao12345@gmail.com>
2025-10-20 17:44:46 -0300

    update caminho do server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+const express = require('express');
+const path = require('path');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+const server = express();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+// Agora você expõe a pasta Images como endpoint público
+server.use('/Images', express.static(path.join(__dirname, 'Images')));
 server.use(router);
+
 server.listen(3000, () => {
   console.log('JSON Server is running');
 });
